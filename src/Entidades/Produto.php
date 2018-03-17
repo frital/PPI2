2e53d41cf6b63ceecd059995b47ebed2083703dc
iftm <iftm@labXX-mXX.laboratorios>
2018-03-16 22:15:56 -0300

    inclusão de componete http

---
+++ src/Entidades/Produto.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PPI2\Entidades;
+
+class Produto {
+    public function msg(){
+        echo 'msg inicial';
+    }
+}
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,4 +1,22 @@
 <?php
+namespace PPI2;
 
-echo 'olá do bootstrap';
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$response = Response::create();
+
+$conteudo = '<h1>Vai corinthians</h1>';
+$response->setContent($conteudo);
+$response->send();
+
+
+
+
+
+
+
 
